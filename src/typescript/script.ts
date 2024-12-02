4e27abc1768623f387e2ad6ed3e0c2dc8c29c9e6
anass17 <anassboutaib2018@gmail.com>
2024-12-02 10:09:31 +0100

    Updated

--- src/typescript/script.ts
+++ src/typescript/script.ts
@@ -774,3 +774,16 @@ formationSelect.addEventListener('change', function () {
     updateLocalStorage();
     
 });
+
+
+
+
+// modal.querySelector('#position')?.addEventListener('change', function (this: HTMLElement) {
+//     if (this.value == "GK") {
+//         document.querySelector('.stat2-container')?.computedStyleMap.display = "block";
+//         document.querySelector('.stat1-container')?.computedStyleMap.display = "";
+//     } else {
+//         document.querySelector('.stat1-container')?.computedStyleMap.display = "block";
+//     }
+    
+// });
